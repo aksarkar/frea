5eb568d508165a7a886db90425e6cb580863dcaf
Abhishek Sarkar <aksarkar@mit.edu>
2016-02-19 15:29:58 -0500

    Implement motif cofactor enrichment
    There are too many cofactors to try and visualize all at once across all
    phenotypes being tested.
    
    Instead, produce one plot per phenotype so we can show each master
    regulator and each cofactor.

--- R/heatmap.R
+++ R/heatmap.R
@@ -1,7 +1,9 @@
 requireNamespace('Cairo')
 requireNamespace('ggplot2')
+requireNamespace('gdata')
 requireNamespace('grid')
 requireNamespace('gtable')
+requireNamespace('reshape2')
 
 #' @include scales.R theme_nature.R
 NULL
@@ -14,6 +16,10 @@ scale_heatmap <- function(...) {
 
 heatmap <- function(plot) {
     plot + geom_raster() + coord_fixed() + theme_nature
+}
+
+sparse_heatmap <- function(plot) {
+    heatmap(plot) + theme(panel.grid.major=element_line(color='gray90'))
 }
 
 epigenome_by_tissue <- 
@@ -68,6 +74,34 @@ enrichment_by_cluster <- function(enrichments, cluster_density) {
     do.call(cbind, c(grobs, size='last'))
 }
 
+master_regulator_by_cofactor <- function(cofactors) {
+    cofactors$V3 <- droplevels(cofactors$V3)
+    cofactors$V4 <- droplevels(cofactors$V4)
+    match_matrix <- acast(cofactors, formula=V3 ~ V4, value.var='V5', fill=0, fun.aggregate=max)
+    row_order <- hclust(dist(match_matrix))$order
+    cofactors$V3 <- gdata::reorder.factor(cofactors$V3, new.order=row_order)
+    col_order <- apply(match_matrix, 2, function (x) {row_order[which.max(x)]})
+    cofactors$V4 <- gdata::reorder.factor(cofactors$V4, new.levels=col_order)
+    (sparse_heatmap(ggplot(cofactors, aes(x=V4, y=V3, fill=V5))) +
+     scale_heatmap() +
+     scale_x_discrete(name='Cofactor', limits=levels(cofactors$V4)) +
+     scale_y_discrete(name='Master regulator', limits=rev(levels(cofactors$V3))) +
+     theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1)))
+}
+
+master_regulator_by_phenotype <- function(enrichments) {
+    enrichment_matrix <- acast(enrichments, formula=V1 ~ V3, value.var='V4', fill=0, fun.aggregate=max)
+    row_order <- hclust(dist(enrichment_matrix))$order
+    enrichments$V1 <- with(enrichments, factor(V1, levels=row.names(enrichment_matrix)[order(row_order)]))
+    col_order <- apply(enrichment_matrix, 2, function (x) {row_order[which.max(x)]})
+    enrichments$V3 <- with(enrichments, factor(V3, levels=levels(V3)[order(col_order)]))
+    (sparse_heatmap(ggplot(enrichments, aes(x=V3, y=V1, fill=log10(V4)))) +
+     scale_heatmap() +
+     scale_y_discrete(limits=rev(levels(enrichments$V1))) +
+     labs(x='Master regulator', y='Phenotype') +
+     theme(axis.text.x=element_text(angle=90, vjust=0.5, hjust=1)))
+}
+
 preview <- function() {
     data(honeybadger2_impute_p2_cluster_density)
     enrichments <- read.delim('/broad/compbio/aksarkar/projects/gwas/wtccc1/EC21/results/matched/hb2-imputed-features/sig', header=F, sep=' ')
@@ -75,3 +109,25 @@ preview <- function() {
     grid.draw(enrichment_by_cluster(enrichments, honeybadger2_impute_p2_cluster_density))
     dev.off()
 }
+plot_motif_enrichments <- function(filename) {
+    data(honeybadger_cluster_density)
+    enrichments <- read.delim(gzfile(filename), header=FALSE, sep=' ')
+    enrichments$V1 <- toupper(sub('^[^-]*-', '', enrichments$V1))
+    enrichments$V2 <- factor(enrichments$V2, levels=row.names(honeybadger_cluster_density))
+    Cairo(type='pdf', file=sub('.txt.gz$', '.pdf', filename), width=120, height=50, units='mm')
+    print(master_regulator_by_phenotype(enrichments))
+    dev.off()
+}
+
+plot_motif_cofactors <- function(filename) {
+    data(honeybadger_cluster_density)
+    cofactors <- read.delim(gzfile(filename), header=FALSE, sep=' ')
+    cofactors$V1 <- toupper(sub('^[^-]*-', '', cofactors$V1))
+    cofactors$V2 <- factor(cofactors$V2, levels=row.names(honeybadger_cluster_density))
+    d_ply(cofactors, .(V1),
+          function (x) {
+              Cairo(type='pdf', file=sprintf('%s/%s-cofactors.pdf', dirname(filename), x$V1[1]),
+                    width=120, height=60, units='mm')
+              print(master_regulator_by_cofactor(x))
+              dev.off()
+          })}
---
+++ frea/motifs/enrichments.py
@@ -0,0 +1,59 @@
+import collections
+import glob
+import itertools
+import operator
+import sys
+
+Enrichment = collections.namedtuple('Enrichment', ['cluster', 'motif', 'odds_ratio', 'p'])
+Cofactor = collections.namedtuple('Cofactor', ['motif', 'score'])
+
+def parse_motif_matches(filename):
+    """Yield (master regulator, cofactor, score) tuples"""
+    result = collections.defaultdict(list)
+    with open(filename) as f:
+        data = (line.split() for line in f)
+        key = operator.itemgetter(0, 1)
+        for k, g in itertools.groupby(sorted(data, key=key), key=key):
+            master_regulator, cofactor = k
+            result[master_regulator].append(Cofactor(cofactor, max(x[6] for x in g)))
+    return result
+
+def parse_motif_enrichments(filename):
+    """Yield (cluster, motif, odds, p) tuples
+
+    Take the motif with best p-value as cluster representative. This assumes
+    input data is already appropriately sorted.
+
+    """
+    with open(filename) as f:
+        data = (line.split() for line in f)
+        for k, g in itertools.groupby(data, key=operator.itemgetter(0)):
+            yield Enrichment(*next(iter(g)))
+
+def generate_long_form():
+    """Generate long-form tuples for use in plotting"""
+    for filename in glob.glob('*.summary'):
+        phenotype, cluster, *_ = filename.split('.')
+        cofactors = parse_motif_matches('{}.{}.matches'.format(phenotype, cluster))
+        for enrichment in parse_motif_enrichments(filename):
+            for cofactor in cofactors[enrichment.motif]:
+                yield phenotype, cluster, motif_to_cluster[enrichment.motif], motif_to_cluster[cofactor.motif], enrichment, cofactor
+
+def output_long_form(output_key):
+    """Output long-form tuples for use in plotting"""
+    key = operator.itemgetter(2, 3)
+    data = sorted(generate_long_form(), key=key)
+    for k, g in itertools.groupby(data, key=key):
+        for record in g:
+            phenotype, cluster, _, _, enrichment, cofactor = record
+            print(phenotype, cluster, *output_key(enrichment, cofactor))
+    
+def output_enrichments():
+    """Output (phenotype, cluster, motif, odds, p) records"""
+    output_long_form(lambda enrichment, _: enrichment)
+
+def output_cofactors():
+    """Output (phenotype, cluster, master regulator, cofactor, score) records"""
+    def output_key(enrichment, cofactor):
+        return enrichment.motif, cofactor.motif, cofactor.score
+    output_long_form(output_key)
