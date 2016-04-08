17dd63f6df7146e3b8ae095c582ee420a4f047cc
Abhishek Sarkar <aksarkar@mit.edu>
2016-04-08 12:02:48 -0400

    Revert "Handle enrichments under controls"
    This control doesn't make sense since it removes too much of the genome.
    
    This reverts commit 22c162220293735340b9222c00a34e8570857e79.
    
    Conflicts:
    	R/enrichment.R

--- R/enrichment.R
+++ R/enrichment.R
@@ -7,10 +7,9 @@ requireNamespace('gtable')
 #' @importFrom grid unit
 NULL
 
-enrichment_by_cluster <- function(enrichments, cluster_density, scale_name) {
-    (heatmap(ggplot(enrichments, aes(x=cluster, y=pheno, fill=val))) +
-     scale_heatmap(name=scale_name) +
-     scale_y_discrete(limits=rev(levels(enrichments$pheno))) +
+enrichment_by_cluster <- function(enrichments, cluster_density) {
+    (heatmap(ggplot(enrichments, aes(x=cluster, y=pheno, fill=(V5 - V6)/ sqrt(V7)))) +
+     scale_heatmap(name='z-score') +
      labs(x='Enhancer module', y='Phenotype') +
      theme_nature +
      theme(axis.title.x=element_blank(),
@@ -58,17 +57,14 @@ plot_enhancer_enrichments <- function(filename, cluster_density, plot_log_fold=F
     enrichments <- parse_enhancer_enrichments(filename)
     enrichments$cluster <- factor(enrichments$V3, levels=row.names(cluster_density))
 
-    if (plot_log_fold) {
-        enrichments <- transform(enrichments, val=log10(V5 / V6))
-        scale_name <- "Log fold enrichment"
-    }
-    else {
-        enrichments <- transform(enrichments, val=(V5 - V6)/ sqrt(V7))
-        scale_name <- "z-score"
-    }
-    my_gtable <- gtable:::rbind.gtable(ggplotGrob(enrichment_by_cluster(enrichments, cluster_density, scale_name)),
-                                       ggplotGrob(density_by_cluster(cluster_density, keep=unique(enrichments$cluster))),
+    my_density <- (density_by_cluster(cluster_density, keep=unique(enrichments$cluster)) +
+                   theme(legend.position='right',
+                         axis.title.x=element_blank(),
+                         axis.text.y=element_text(margin=margin(2))))
+    my_gtable <- gtable:::rbind.gtable(ggplotGrob(enrichment_by_cluster(enrichments, cluster_density)),
+                                       ggplotGrob(my_density),
                                        size='last')
+
     ## Add tissue colors
     cluster_by_tissue_grob <- ggplotGrob(cluster_by_tissue(cluster_density, keep=unique(enrichments$cluster)))
     my_gtable <- gtable::gtable_add_rows(my_gtable, unit(4, 'mm'), 7)
@@ -79,16 +75,7 @@ plot_enhancer_enrichments <- function(filename, cluster_density, plot_log_fold=F
     my_legend <- roadmap_tissue_legend(list(legend.position='bottom'), list(direction='horizontal', nrow=2))
     my_gtable <- gtable::gtable_add_grob(my_gtable, my_legend, t=-1, l=1, r=-1)
 
-    if (flip) {
-        w <- 270
-        h <- 210
-    }
-    else {
-        w <- 210
-        h <- 270
-    }
-
-    Cairo(type='pdf', file=sub('.in$', '.pdf', filename), width=w, height=h, units='mm')
+    Cairo(type='pdf', file=sub('.in$', '.pdf', filename), width=210, height=270, units='mm')
     grid::grid.draw(my_gtable)
     dev.off()
 }
