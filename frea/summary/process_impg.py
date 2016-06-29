ce39b02e0810bd97f272520db8f0212d3ed0f982
Abhishek Sarkar <aksarkar@mit.edu>
2016-06-29 09:55:51 -0400

    Fix broken refactor

--- frea/summary/process_impg.py
+++ frea/summary/process_impg.py
@@ -1,20 +1,37 @@
+"""Convert ImpG output to UCSC BED
+
+Usage: python -m frea.process.process_impg CHROMOSOME
+
+Assumes current working directory contains ImpG formatted output (including
+r2pred) files with ImpG naming scheme (chrX.start-end.txt).
+
+Author: Abhishek Sarkar <aksarkar@mit.edu>
+
+"""
 import functools
+import os
 import re
 import signal
+import sys
 
 import scipy.stats
 
-from .join import *
+from ..algorithms import join
+from ..formats import parse, impg_format
+from .lookup import *
 
 def impg_window(filename, min_r2pred=0.6):
     patt = re.compile(r'\d+\.(?P<start>\d+)-(?P<end>\d+)')
     m = patt.match(filename)
+    target = '{}.txt'.format(m.group(0))
     start, end = m.groups()
     start = int(start)
     end = int(end) - 250000
     if start > 1:
         start += 250000
-    with open('{}.txt'.format(m.group(0))) as f:
+    if not os.path.exists(target):
+        raise StopIteration
+    with open(target) as f:
         data = (line.split() for line in f)
         next(data)
         parsed = parse(impg_format, data)
@@ -28,15 +45,19 @@ def impg_chromosome(chromosome, min_r2pred=0.6):
             for row in impg_window(filename, min_r2pred):
                 yield row
 
-def process_impg(chromosome):
+def process_impg(chromosome, ref_dir='.'):
     """Process ImpG output"""
     sf = scipy.stats.chi2(1).sf
     k = 'chr{}'.format(chromosome)
     output_ = functools.partial(output, key=lambda x: (k, sf(math.pow(x[4], 2))))
     lookup(impg_chromosome(chromosome), output_fn=output_,
            input_sort_key=lambda x: k, input_join_key=operator.itemgetter(1, 2, 3),
-           ref_join_key=operator.itemgetter(1, 2, 3))
+           ref_join_key=operator.itemgetter(1, 2, 3), ref_dir=ref_dir)
 
 if __name__ == '__main__':
     signal.signal(signal.SIGPIPE, signal.SIG_DFL)
-    process_impg(sys.argv[1])
+    if len(sys.argv) > 2:
+        ref_dir = sys.argv[2]
+    else:
+        ref_dir = '.'
+    process_impg(sys.argv[1], ref_dir)
