82482f9bab74dac6b812c41d82baa147ad24f858
Abhishek Sarkar <aksarkar@mit.edu>
2016-06-30 14:46:01 -0400

    Use subcohort ranking for rank-based correlation
    This avoids a bias where the full ranking induces correlation between
    the subcohorts

--- frea/rank.py
+++ frea/rank.py
@@ -33,7 +33,9 @@ import itertools
 import operator
 import sys
 
-def pearson(cohort):
+from .algorithms import moments
+
+def pearson(eps=1e-8):
     """Coroutine implementation of Pearson correlation
 
     https://groups.google.com/d/msg/sci.stat.math/HMrw3pWSvQs/-QP3BehUV1MJ
@@ -42,18 +44,13 @@ def pearson(cohort):
     n = 0
     mean_x = 0
     mean_y = 0
-    sum_x_sq = 0
-    sum_y_sq = 0
+    sum_x_sq = eps
+    sum_y_sq = eps
     sum_xy = 0
-    def output():
-        print(n, cohort, sum_xy / math.sqrt(sum_x_sq * sum_y_sq))
-    cutoffs = set(x * y for x, y in itertools.product([1, 2, 3, 4, 5], [10 ** i for i in range(3, 7)]))
     try:
         while True:
-            x, y = yield
+            x, y = yield sum_xy / math.sqrt(sum_x_sq * sum_y_sq)
             n += 1
-            if n in cutoffs:
-                output()
             dev_x = x - mean_x
             dev_y = y - mean_y
             mean_x += dev_x / n
@@ -62,36 +59,54 @@ def pearson(cohort):
             sum_y_sq += dev_y * (y - mean_y)
             sum_xy += dev_x * (y - mean_y)
     except GeneratorExit:
-        output()
+        return sum_xy / math.sqrt(sum_x_sq * sum_y_sq)
 
-def hold_out_cor(zscores, cohorts):
-    targets = [pearson(cohort) for cohort in cohorts] + [pearson("Overall")]
-    for t in targets:
-        next(t)
-    for row in zscores:
-        sample_weighted = sum(x * w for x, w in zip(row, weights))
-        for z, w, t in zip(row, weights, targets):
-            t.send((z, (sample_weighted - z * w) / (total_weight - w)))
-        targets[-1].send((row[-1], sample_weighted / total_weight))
-    for t in targets:
-        t.close()
-
-cohorts = ["WTCCC", "NARAC1", "NARAC2", "EIRA", "CANADA", "BRASS"]
-samples = [(1525, 10608),  
-           (867, 1041),    
-           (902, 4510),    
-           (1173, 1089),   
-           (489, 1472),    
-           (483, 1449)]    
-total_samples = sum(sum(cohort) for cohort in samples)
-weights = [math.sqrt(sum(cohort) / total_samples) for cohort in samples]
-total_weight = sum(weights)
-
-with gzip.open(sys.argv[1], "rt") as f:
-    data = (line.split() for line in f)
+def parse(file):
+    data = (line.split() for line in file)
     next(data)  # skip header
     filter_chr6 = (row for row in data if row[1] != '6')
     projection = (row[11:17] + row[-4:-3] for row in filter_chr6)
     filter_missing = (row for row in projection if 'NA' not in row)
-    zscores = sorted(([float(x) for x in row] for row in filter_missing), key=operator.itemgetter(-1))
-    hold_out_cor(zscores, cohorts)
+    return [[float(x) for x in row] for row in filter_missing]
+
+def running_corr(zscores, hold_out=-1):
+    cutoffs = set(x * y for x, y in itertools.product([1, 2, 3, 4, 5], [10 ** i for i in range(3, 7)]))
+    p = pearson()
+    p.send(None)
+    for i, row in enumerate(sorted(zscores, key=operator.itemgetter(hold_out))):
+        sample_weighted = sum(x * w for x, w in zip(row, weights))
+        r = p.send((row[hold_out], (sample_weighted - row[hold_out] * weights[hold_out]) /
+                    (total_weight - weights[hold_out])))
+        if i in cutoffs:
+            yield i, r
+            p.close()
+
+def componentwise_corr(zscores, hold_out=-1):
+    for i, cohort in enumerate(cohorts):
+        for row in sorted(zscores, key=operator.itemgetter(i)):
+            sample_weighted = sum(x * w for x, w in zip(row, weights))
+            yield row[hold_out] * (sample_weighted - row[hold_out] * weights[hold_out]) / (total_weight - weights[hold_out])
+
+if __name__ == '__main__':
+    cohorts = ["WTCCC", "NARAC1", "NARAC2", "EIRA", "CANADA", "BRASS"]
+    samples = [(1525, 10608),
+               (867, 1041),
+               (902, 4510),
+               (1173, 1089),
+               (489, 1472),
+               (483, 1449)]
+
+    total_samples = sum(sum(cohort) for cohort in samples)
+    # Add sentinel for overall correlation
+    weights = [math.sqrt(sum(cohort) / total_samples) for cohort in samples] + [0]
+    total_weight = sum(weights)
+
+    with gzip.open(sys.argv[1], "rt") as f:
+        zscores = parse(f)
+        for n, r in running_corr(zscores):
+            print(n, 'Overall', r)
+        for i, cohort in enumerate(cohorts):
+            for n, r in running_corr(zscores, hold_out=i):
+                print(n, cohort, r)
+                numpy.sqrt(sum(samples[i]) * sum(sum(x) for j, x in enumerate(samples) if i != j)) * .6 / len(zscores)
+                print(*moments(componentwise_corr(zscores, hold_out=i)), file=sys.stderr)
