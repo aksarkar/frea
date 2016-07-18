21f63c14fe19a4d05e8adc67ff7f141fa551015a
Abhishek Sarkar <aksarkar@mit.edu>
2016-07-18 19:51:26 -0400

    Use recombination hotspots instead of fixed points

--- frea/simulation.py
+++ frea/simulation.py
@@ -1,12 +1,15 @@
 """Simulate phenotypes with different genetic architectures.
 
+Usage: python -m frea.simulation SEED N LEGEND HAPS SAMPLE
+
 Author: Abhishek Sarkar <aksarkar@mit.edu>
 
 """
+import argparse
 import collections
 import itertools
 import gzip
-import os.path
+import pickle
 import random
 import sys
 
@@ -15,60 +18,116 @@ import numpy.random as R
 
 from .formats import *
 
-def windows(key, ref_dir='.', p_causal=0.5, window_size=1e6, n_per_window=1, **kwargs):
-    J = os.path.join
-    for chr_ in [str(i) for i in range(1, 23)]:
-        sample_file = J(ref_dir, 'ALL.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.sample')
-        legend_file = J(ref_dir, 'ALL.chr{}.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing.legend.gz'.format(chr_))
-        haps_file = J(ref_dir, 'ALL.chr{}.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing.haplotypes.gz'.format(chr_))
-        with oxstats_haplotypes(sample_file, legend_file, haps_file, **kwargs) as data:
-            for _, g in itertools.groupby(data, key=key):
-                yield g
+def blocks(hotspot_file, **kwargs):
+    """Yield LD blocks defined by recombination hotspots
 
-def sample_gaussian(n, pve=0.5, ref_dir='.', p_causal=0.5, window_size=1e6,
-                    n_per_window=1, **kwargs):
-    """Generate Gaussian phenotypes.
+    Blocks are pairs of iterables (instances of itertools.takewhile), allowing
+    sequential access to OXSTATS reference haplotypes, and recombination rates
+    at the block boundary (next recombination). Hotspots are taken as the
+    center of contiguous 10kb regions with significantly different
+    recombination rate from flanking regions. Recombination rates are taken as
+    the mean of standardized recombination rates estimated in each 10kb window,
+    in units of percentage of haplotypes broken by a recombination event.
 
-    This implementation recombines reference haplotypes to generate arbitrary
-    size samples. We make two passes through the reference data:
-
-    1. Sample causal variants, effect sizes, and recombinations (at fixed
-       intervals); reconstruct locally to accumulate liabilities and genetic
-       variance
-
-    2. Add Gaussian noise; reconstruct the sample genotypes and compute
-       marginal association statistics
-
-    n - target number of samples
-    pve - target proportion of variance explained
-    ref_dir - base directory of 1KG reference haplotypes
-    kwargs - keyword arguments to oxstats_haplotypes
+    hotspot_file - deCODE recombination hotspot file (single chromosome)
+    kwargs - arguments to oxstats_haplotypes
 
     """
-    key = lambda x: int(int(x[0][1]) / window_size)
+
+    with decode_recombination_hotspots(hotspot_file) as hotspots, oxstats_haplotypes(**kwargs) as data:
+        for pos, rate in hotspots:
+            yield itertools.takewhile(lambda x: int(x[0][1]) < pos, data), rate
+        yield data, 0
+
+def _reconstruct(mosaic, haplotypes):
+    """Return centered genotypes (n x p) according to ancestor pointers
+
+    haplotypes - list of list of haplotypes (p x k)
+    mosaic - list of ancestor pointers (2n x 1; values in 1..k)
+
+    """
+    w = numpy.array(haplotypes, dtype='int8').T[mosaic]  # 2n x p
+    x = (w[::2] + w[1::2]).astype('float32')
+    x -= x.mean(axis=0)
+    return x
+
+def sample_events(seed, n, hotspot_file, p_causal=0.5, n_per_window=1,
+                  **kwargs):
+    """Return recombination events and estimated liabilities
+
+    The goal is to recombine reference haplotypes to generate genotypes and
+    phenotypes for arbitrary size samples in two passes through the reference
+    data.
+
+    In the first pass, sample causal variants, effect sizes, and recombinations
+    (at recombination hotspots). Recombinations are stored as an initial
+    configuration plus sparse updates. Causal variants and effect sizes are
+    stored as genetic values (since we won't need the actual effects later).
+
+    mosaic - initial ancestor pointers (2n x 1; values in 1..k)
+    p_causal - probability each block contains a causal variant
+    n_per_window - number of causal variants per block
+    hotspot_file - deCODE recombination hotspot file (single chromosome)
+    kwargs - arguments to oxstats_haplotypes
+
+    """
+    mosaic = None
+    events = []
     y = numpy.zeros(n)
-    mosaic = []
-    ## Pass 1
-    for window in windows(key, ref_dir=ref_dir, **kwargs):
-        window = numpy.array([h for _, h in window])
-        p, k = window.shape
-        mosaic.append(R.randint(0, k, size=2 * n))
+    for block, rate in blocks(hotspot_file, **kwargs):
+        haplotypes = [h for _, h in block]
+        if len(haplotypes) == 0:
+            break
+        haplotypes = numpy.array(haplotypes, dtype='int8')
+        p, k = haplotypes.shape
+        if mosaic is None:
+            R.seed(seed)
+            mosaic = R.randint(0, k, size=2 * n)
         if R.rand() < p_causal:
             causal = R.choice(numpy.arange(p), size=n_per_window, replace=False)
             effects = R.normal(size=n_per_window)
-            w = window.T[mosaic[-1], causal]  # 2n x n_per_window
-            x = w[::2] + w[1::2]
-            y += x.reshape(-1, n_per_window).dot(effects)
-    y += R.normal(scale=(1 / pve - 1) * y.std(), size=n)
-    y -= y.mean()
-    y -= y.std()
-    ## Pass 2
+            x = _reconstruct(mosaic, haplotypes)
+            y += x[:,causal].dot(effects)
+        # TODO: without rejection sampling, the sampled rate of recombinations
+        # will be biased towards zero. But accurately simulating recombination
+        # is not the goal of this simulation, so just verify that the summary
+        # statistics are reasonable
+        hits = numpy.where(R.uniform(size=2 * n) < rate / 100)[0]
+        ancestors = R.randint(0, k, size=hits.shape)
+        events.append((hits, ancestors))
+        mosaic[hits] = ancestors
+    return y, events
+
+def marginal_association(seed, y, mosaic, events, hotspot_file_file, pve=0.5,
+                         eps=1e-8, **kwargs):
+    """Output marginal association statistics for a Gaussian phenotype
+
+    In the second pass, reconstruct the sample genotypes locally and compute
+    marginal association statistics in parallel using matrix operations. Output
+    summary statistics in BED format, with -log10(p) as the score and a
+    placeholder (',') for chromosome. The placeholder is needed to avoid
+    dependency on file name formatting, which changes with each 1KG release
+
+    Assume the same random seed is used as in the first pass (to get the same
+    starting configuration).
+
+    mosaic - initial ancestor pointers (2n x 1; values in 1..k)
+    events - list of (haplotype index, new ancestor) updates to mosaic
+    hotspot_file - deCODE recombination hotspot file (single chromosome)
+    pve - target proportion of variance explained
+    eps - tolerance (for SNP variance at monomorphic SNPs)
+    kwargs - arguments to oxstats_haplotypes
+
+    """
+    n = y.shape
+    R.seed(seed)
+    mosaic = R.randint(0, k, size=2 * n)
     numpy.seterr(all='warn')
-    for ancestors, window in zip(mosaic, windows(key, ref_dir=ref_dir, **kwargs)):
-        window = list(window)
-        w = numpy.array([h for _, h in window]).T[ancestors]  # 2n x p
-        x = (w[::2] + w[1::2]).astype('float32')
-        x -= x.mean(axis=0)
+    for (block, _), event in zip(blocks(hotspot_file, **kwargs), events):
+        block = list(block)
+        hits, ancestors = event
+        mosaic[hits] = ancestors
+        x = _reconstruct(mosaic, [h for _, h in window])
         var = numpy.diag(x.T.dot(x)) + 1e-8  # Needed for monomorphic SNPs
         b = y.T.dot(x).T / var
         rss = numpy.inner(y - x.dot(b), y - x.dot(b))
@@ -76,7 +135,24 @@ def sample_gaussian(n, pve=0.5, ref_dir='.', p_causal=0.5, window_size=1e6,
         stat = numpy.square(b / se)
         logp = -scipy.stats.chi2(1).logsf(stat)
         for (l, _), p in zip(window, logp):
-            print(' '.join(l), p)
+            name, pos, a0, a1, *_ = l
+            print(output_ucsc_bed(',', p, name, int(pos), a0, a1))
+
+def combine_genetic_values(files, pve=0.5):
+    """Add Gaussian noise to generate a phenotype with target PVE"""
+    genetic_values = []
+    for f_ in files:
+        with open(f_, 'rb') as f:
+            y, _ = pickle.load(f)
+            genetic_values.append(y)
+    y = sum(genetic_values)
+    # Use the realized genetic variance to set the noise scale. In other
+    # simulations, we use the population value of the genetic variance (based
+    # on MAF and effect size) instead.
+    y += R.normal(scale=(1 / pve - 1) * y.std(), size=n)
+    y -= y.mean()
+    y -= y.std()
+    return y
 
 def sample_uniform(data, p_causal=0.5, window_size=1e6, n_per_window=1):
     for _, g in itertools.groupby(data, key=lambda x: (x[0], int(int(x[2]) / window_size))):
@@ -105,10 +181,3 @@ def simulate_null(samples, probs, pve=0.5, **kwargs):
     print(' '.join(samples[1]), 'P')
     for i, y_i in enumerate(y):
         print(' '.join(samples[i + 2]), y_i)
-
-if __name__ == '__main__':
-    random.seed(sys.argv[1])
-    if sys.argv[2] == '-':
-        sys.argv[2] = None
-    with oxstats(sys.argv[3], sys.argv[2]) as (samples, probs):
-        simulate_null(samples, probs)
--- frea/tests/test_simulation.py
+++ frea/tests/test_simulation.py
@@ -5,30 +5,42 @@ import random
 
 import pytest
 
-from frea.algorithms import kwise
-from frea.formats import *
 from frea.simulation import *
 
 @pytest.fixture
 def set_working_dir():
-    os.chdir('/broad/compbio/aksarkar/projects/roadmap/data/1kg/')
+    os.chdir('/broad/hptmp/aksarkar/dummy')
     sample_file = 'ALL.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.sample'
     legend_file = 'ALL.chr1.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing.legend.gz'
     haps_file = 'ALL.chr1.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing.haplotypes.gz'
-    random.seed(0)
-    return sample_file, legend_file, haps_file
+    hotspot_file = 'decode-chr1.txt.gz'
+    return sample_file, legend_file, haps_file, hotspot_file
 
-def test_sample_chromosome_mosaic(set_working_dir):
-    sample_file, legend_file, haps_file = set_working_dir
-    ancestors, variants, effects = next(sample_chromosome_mosaic(10, sample_file, legend_file, haps_file))
-    assert len(ancestors) == 20
+def test_blocks_data(set_working_dir):
+    sample_file, legend_file, haps_file, hotspot_file = set_working_dir
+    iterable = blocks(hotspot_file, sample_file=sample_file,
+                      legend_file=legend_file, haps_file=haps_file)
+    block, rate = next(iterable)
+    block = list(block)
+    import pdb; pdb.set_trace()
+    assert len(block) == 21069
 
-def test_reconstruct_chromosome_mosaic(set_working_dir):
-    sample_file, legend_file, haps_file = set_working_dir
-    with oxstats_haplotypes(sample_file, legend_file, haps_file) as data:
-        expected_haps = [h[:20] for l, h in itertools.islice(data, 0, 10)]
-        expected_genotypes = [[sum(pair) for pair in kwise(h, 2)] for h in expected_haps]
-    ancestors = list(range(20))
-    reconstuction = reconstruct_chromosome_mosaic(ancestors, sample_file, legend_file, haps_file)
-    genotypes = list(itertools.islice(reconstuction, 0, 10))
-    assert genotypes == expected_genotypes
+def test_blocks_final_block(set_working_dir):
+    sample_file, legend_file, haps_file, hotspot_file = set_working_dir
+    l = 0
+    for block, rate in blocks(hotspot_file, sample_file=sample_file,
+                              legend_file=legend_file, haps_file=haps_file):
+        l = len(list(block))
+    assert l == 10136
+
+def test_blocks_rate(set_working_dir):
+    sample_file, legend_file, haps_file, hotspot_file = set_working_dir
+    block, rate = next(blocks(hotspot_file, sample_file=sample_file,
+                              legend_file=legend_file, haps_file=haps_file))
+    assert numpy.isclose(rate, 12.227070)
+
+def test_sample_events(set_working_dir):
+    sample_file, legend_file, haps_file, hotspot_file = set_working_dir
+    y, events = sample_events(0, 100, hotspot_file, sample_file=sample_file,
+                              legend_file=legend_file, haps_file=haps_file)
+    assert y.shape == (100,)
