4e17e6a7bfadce57e06320dd522e8d9578fb15f7
Abhishek Sarkar <aksarkar@mit.edu>
2016-09-05 17:34:15 -0400

    Support sharding the reference LD information
    Port old code into the public package

--- frea/ld/shard.py
+++ frea/ld/shard.py
@@ -3,14 +3,26 @@
 Author: Abhishek Sarkar
 
 """
+import argparse
 import gzip
 import itertools
 import operator
 import sys
 
-with gzip.open(sys.argv[1], 'rt') as f:
+parser = argparse.ArgumentParser()
+parser.add_argument('-r', '--ref', action='store_true',
+                    help='Shard reference LD information')
+parser.add_argument('file', help='File to split by chromosome')
+args = parser.parse_args()
+
+if args.ref:
+    key = lambda x: x[0].split('|')[1]
+else:
+    key=operator.itemgetter(0)
+
+with gzip.open(args.file, 'rt') as f:
     data = (line.split() for line in f)
-    for k, g in itertools.groupby(data, key=operator.itemgetter(0)):
+    for k, g in itertools.groupby(data, key=key):
         with gzip.open('{}.txt.gz'.format(k), 'wt') as out:
             for row in g:
                 print(*row, file=out)
